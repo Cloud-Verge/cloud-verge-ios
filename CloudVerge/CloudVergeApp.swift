1cd8070bc7dcc199a45d281e4e2726d0e91fcf1f
Кириллов Артемий Михайлович <amikhkirillov@sberbank.ru>
2024-05-01 16:35:00 +0300

    Initial Commit

---
+++ CloudVerge/CloudVergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudVergeApp.swift
+//  CloudVerge
+//
+//  Created by Кириллов Артемий Михайлович on 01.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct CloudVergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
